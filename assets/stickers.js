8569e1d8a4f91d87f788063f518b576d421fc960
MItPlaza <m.plazaweb@gmail.com>
2024-01-20 00:24:10 -0300

    cambiamos el input

--- assets/stickers.js
+++ assets/stickers.js
@@ -1,5 +1,3 @@
-
-
 let cantidad1 = document.querySelector('#sticker1');
 let cantidad2 = document.querySelector('#sticker2');
 let cantidad3 = document.querySelector('#sticker3');
@@ -8,10 +6,13 @@ let verificar = document.querySelector('#verificarStickers');
 let mensaje = document.querySelector('#txtInserto');
 
 verificar.addEventListener('click', () => {
-    // Obtener los valores como números
-    let valor1 = parseInt(cantidad1.value) || 0; // Si no es un número válido, se establece a 0
+
+  // Obtener los valores como números
+    let valor1 = parseInt(cantidad1.value) || 0; // Si no es un número válido, se establece a 0  
     let valor2 = parseInt(cantidad2.value) || 0;
     let valor3 = parseInt(cantidad3.value) || 0;
+
+   
 
     // Realizar la suma
     let sumarStickers = valor1 + valor2 + valor3;
